7a49bff4e3147d44908625625fd095d3537f1627
稲員未来 <miku.inakazu@gmail.com>
2017-11-18 16:33:10 +0900

    associated meetings table and users table, removed name column and added user_id column to meetings table

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,4 @@
 class Meeting < ActiveRecord::Base
+  belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117125359) do
+ActiveRecord::Schema.define(version: 20171118070713) do
 
   create_table "meetings", force: :cascade do |t|
-    t.string   "name",       limit: 255
     t.datetime "start_time"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "shift_users", force: :cascade do |t|
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20171117125359) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
