90675f5e6df29afb9eff6eb0e610ab3f0133e3ab
稲員未来 <miku.inakazu@gmail.com>
2017-11-25 15:51:45 +0900

    made sign_up. log_in and log_out session. if the user is not logged in, reidrected to log_in screen.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   def after_sign_out_path_for(resource)
-    new_user_path
+    new_user_session_path
   end
 end
